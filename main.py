c3c3ed1ab8989c558acdda7b863679009b7e37b8
Shengyou Jiang <sjj5492@psu.edu>
2020-09-16 19:16:02 +0000

    adding a //

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def digit_sum(n):
   if n == 0:
     return 0
   else:
-    return (n % 10 + digit_sum(int(n / 10))) 
+    return (n % 10 + digit_sum(int(n // 10))) 
 
 def run():
   num = int(input("Enter an int: "))
